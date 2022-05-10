61052b4623086ef94e8d7ce9b3527f4fc986a754
Ahmad Zaheer <ahmad.zaheer@systemsltd.com>
2022-05-10 10:39:47 +0500

    1. foreground notifications code added

--- app/src/main/java/com/onelink/nrlp/android/services/PushNotificationService.kt
+++ app/src/main/java/com/onelink/nrlp/android/services/PushNotificationService.kt
@@ -1,7 +1,54 @@
 package com.onelink.nrlp.android.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.onelink.nrlp.android.R
+
 
 class PushNotificationService: FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("Token", token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        val title = message.notification?.title
+        val body = message.notification?.body
+        val channelId = getString(R.string.channel_id)
+        sendNotification(title, body, channelId)
+    }
+
+    private fun sendNotification(title: String?, body: String?, channelId: String) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Sohni Dharti Notifications",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(title)
+            //.setContentIntent(pendingIntent)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setSound(uri)
+        notificationManager.notify(System.currentTimeMillis().toInt(), builder.build())
+    }
 
 }
